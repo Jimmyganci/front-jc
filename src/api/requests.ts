acf2cd5709f56a31529c49ed268709fae5a7e137
Jimmy <jimmy.ganci@gmail.com>
2022-04-17 21:20:25 +0200

    test production

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -7,8 +7,9 @@ const API_URL =
     : "https://api-jc.herokuapp.com";
 
 const auth = {
-  loggedIn: ({ email, password }: TypeInputLogin): Promise<TypeInputLogin> =>
-    axios
+  loggedIn: ({ email, password }: TypeInputLogin): Promise<TypeInputLogin> => {
+    console.log(API_URL);
+    return axios
       .post(
         `${API_URL}/auth?API_KEY=${import.meta.env.VITE_API_KEY}`,
         { email, password },
@@ -20,7 +21,8 @@ const auth = {
           withCredentials: true,
         }
       )
-      .then((res) => res.data),
+      .then((res) => res.data);
+  },
 };
 
 export default auth;
